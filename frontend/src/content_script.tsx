6fbc119d8a3f220a71f105498d2814fab41379a3
Faizan Ali <faizanali619@gmail.com>
2023-09-17 16:27:31 -0700

    working?

--- frontend/src/content_script.tsx
+++ frontend/src/content_script.tsx
@@ -13,12 +13,23 @@ const generateImage = async () => {
         await fetch(`http://localhost:3000/api/imagen?text=${p.textContent}`)
           .then(res => res.json())
           .then(data => {
+            const div: HTMLDivElement = document.createElement('div')
             // Create an image element
-            const imgElement = document.createElement('img')
-            imgElement.src = data.url
-            imgElement.alt = 'Loading image...'
+            const img = document.createElement('img')
+            img.src = data.url
+            img.width = 400
+            img.height = 400
+            img.alt = 'Image for paragraph above'
+            div.style.marginTop = '1rem'
+            div.style.marginBottom = '1rem'
+            div.style.display = 'flex'
+            div.style.flexDirection = 'column'
+            div.style.justifyContent = 'center'
+            div.style.alignItems = 'center'
 
-            p!.parentNode!.insertBefore(imgElement, p!.nextSibling)
+            div.appendChild(img)
+
+            p!.parentNode!.insertBefore(div, p!.previousSibling)
           })
       }
     }))
