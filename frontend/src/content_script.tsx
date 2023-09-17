c2cc307b94307aae76484989dfe1ea235c6ba00c
Saketh Nimmagadda <sakethn@cs.washington.edu>
2023-09-17 16:42:03 -0700

    Reduced image frequency - images should appear at every 2500 characters, rounded up to the nearest paragraph

--- frontend/src/content_script.tsx
+++ frontend/src/content_script.tsx
@@ -2,37 +2,33 @@ console.log('Extension loaded')
 
 const generateImage = async () => {
   const pElements = document.querySelectorAll('p')
+  let charCount = 0
 
   console.log('Fetching images in parallel')
+
   await Promise.all(
     Array.from(pElements).map(async (p, index) => {
       console.log(index, p.textContent)
-      if (((index + 1) % 3) === 0) {
-        console.log(`Fetching image for ${index} + 1th element`)
+      charCount += p.textContent?.length ?? 0
+
+      if (charCount >= 2500) {
+        // Reset character count
+        charCount = 0
+        console.log(`Fetching image for chunk ending at ${index + 1}th element`)
 
         await fetch(`http://localhost:3000/api/imagen?text=${p.textContent}`)
           .then(res => res.json())
           .then(data => {
-            const div: HTMLDivElement = document.createElement('div')
             // Create an image element
-            const img = document.createElement('img')
-            img.src = data.url
-            img.width = 400
-            img.height = 400
-            img.alt = 'Image for paragraph above'
-            div.style.marginTop = '1rem'
-            div.style.marginBottom = '1rem'
-            div.style.display = 'flex'
-            div.style.flexDirection = 'column'
-            div.style.justifyContent = 'center'
-            div.style.alignItems = 'center'
+            const imgElement = document.createElement('img')
+            imgElement.src = data.url
 
-            div.appendChild(img)
-
-            p!.parentNode!.insertBefore(div, p!.previousSibling)
+            // Insert the image after the current <p> element
+            p.parentNode?.insertBefore(imgElement, p.nextSibling)
           })
       }
-    }))
+    })
+  )
 }
 
 generateImage().catch(e => console.error('Error from content script'))
